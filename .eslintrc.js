04e6142468daad1f50ee3b067f28062267c284e2
Robin Panta <hacktivistic@gmail.com>
2020-04-27 00:15:26 +0530

    chore: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    // 'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    "indent": ["error", "tab"],
+    "space-before-function-paren": ["error", "always"],
+    "semi": ["error", "never"],
+	"no-tabs": ["error", { allowIndentationTabs: true }],
+  },
+};
