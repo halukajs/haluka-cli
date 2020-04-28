ba290bd0b649b5bf44531c97c2c8bab699f95b56
Robin Panta <hacktivistic@gmail.com>
2020-04-29 01:55:01 +0530

    test: fix description fail due to chalk

--- __test__/commander.spec.js
+++ __test__/commander.spec.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-undef */
 
+const chalk = require('chalk');
+
 const commander = require('../lib/commander');
+
 
 describe('Commander', () => {
   test('should return yargs command options', () => {
@@ -14,7 +17,8 @@ describe('Commander', () => {
 
     expect(newCommand).toHaveProperty('command', 'test_command');
     expect(newCommand).toHaveProperty('alias', ['tc']);
-    expect(newCommand).toHaveProperty('description', 'A test command.');
+    expect(newCommand).toHaveProperty('description', chalk.magentaBright('A test command.'));
     expect(newCommand).toHaveProperty('handler');
+    expect(newCommand.handler).toBeInstanceOf(Function);
   });
 });
