cccb34fe9100648ea80422fd8433a7933a776dad
Robin Panta <hacktivistic@gmail.com>
2020-04-28 06:28:30 +0530

    feat(*): create cli runner

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) Robin Panta
+ * Haluka CLI
+ */
+
+const yargs = require('yargs');
+const chalk = require('chalk');
+
+const errorHandlr = require('./lib/error');
+const VERSION = require('./package.json').version;
+
+try {
+  yargs.commandDir('commands')
+    .usage(`${chalk.bold.underline('Usage:')} ${chalk.green('$0')} ${chalk.yellow('<command>')} ${chalk.blue('[<args>] [<options>]')}`)
+    // .completion('completion', chalk.yellowBright('Command completed.'))
+    .demandCommand()
+    .help('help', chalk.yellowBright('Show help for the command'))
+    .recommendCommands()
+    .scriptName('haluka')
+    .strict()
+    .version('version', chalk.yellowBright('Show version.'), VERSION)
+    .wrap(yargs.terminalWidth())
+    .parse();
+} catch (error) {
+  errorHandlr(error);
+}
