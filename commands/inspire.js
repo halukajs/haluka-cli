d500e26cdb441eb6049aaee0c859d34d008073f1
Robin Panta <hacktivistic@gmail.com>
2020-04-28 06:29:50 +0530

    feat(command): first command 'inspire'
    DIsplays an inspiring quote.

---
+++ commands/inspire.js
@@ -0,0 +1,24 @@
+
+const chalk = require('chalk');
+const command = require('../lib/commander');
+
+const inspirations = [
+  'If Einstein were alive, he would use Haluka.',
+  'Be yourself; everyone else is already taken.',
+  'With Haluka you can achieve the impossible.',
+  'Life is 10% what happens to us and 90% how we react to it.',
+  'When life give you lemons, use Haluka to blend.',
+];
+/**
+ * Copyright (c) Robin Panta
+ * Inspire Command
+ */
+module.exports.inspire = command({
+  command: 'inspire',
+  alias: ['i'],
+  description: 'Inspires you to achieve more.',
+}, () => {
+  console.log();
+  console.log(`     ${chalk.greenBright(inspirations[Math.floor(Math.random() * inspirations.length)])}`);
+  console.log();
+});
