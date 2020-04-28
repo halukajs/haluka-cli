5e82b256cdf03fbd0b2fa002af9cbb6a036595f5
Robin Panta <hacktivistic@gmail.com>
2020-04-29 02:09:20 +0530

    chore: prevent console output while testing

--- __test__/inspire.spec.js
+++ __test__/inspire.spec.js
@@ -4,6 +4,9 @@ const inspireCommand = require('../commands/inspire');
 
 describe('Inspire Command', () => {
   test('should not throw', () => {
+    const cl = console.log;
+    console.log = () => {};
     expect(() => inspireCommand.handler()).not.toThrow();
+    console.log = cl;
   });
 });
