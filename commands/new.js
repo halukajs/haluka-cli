86c668ea4eaf72f5e29f927461fcc2130a235caf
Robin Panta <hacktivistic@gmail.com>
2020-05-01 00:50:58 +0530

    feat(new): Create New Command

---
+++ commands/new.js
@@ -0,0 +1,22 @@
+
+const chalk = require('chalk');
+const fs = require('fs');
+const command = require('../lib/commander');
+const util = require('../lib/util');
+
+
+/**
+ * Copyright (c) Robin Panta
+ * New Command
+ */
+module.exports = command({
+  command: 'new <name>',
+  description: 'Setups a new Haluka project.',
+}, (yargv) => {
+  const projectName = yargv.name;
+  if (fs.existsSync(util.getCurrentPath(projectName))) {
+    console.error(chalk.red(`Error: Cannot create project. \nA file or folder with name '${projectName}' already exists in the active directory.`));
+    process.exit(1);
+  }
+  // safe to create project. Pull project seed from git.
+});
