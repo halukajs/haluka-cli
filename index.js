a3803ecc9c484e24005833be4d061d2875661706
Robin Panta <hacktivistic@gmail.com>
2020-04-30 23:44:36 +0530

    feat: Add time Log to console log

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ const chalk = require('chalk');
 const errorHandlr = require('./lib/error');
 const VERSION = require('./package.json').version;
 
+// Setup Debugger
+const cl = console.log;
+console.log = (...args) => { cl(`[${chalk.blueBright((new Date()).toLocaleTimeString())}]`, ...args); };
+
 try {
-  yargs.commandDir('commands')
+  yargs
+    .commandDir('commands')
     .usage(`${chalk.bold.underline('Usage:')} ${chalk.green('$0')} ${chalk.yellow('<command>')} ${chalk.blue('[args...] [options...]')}`)
     // .completion('completion', chalk.yellowBright('Command completed.'))
     .demandCommand()
