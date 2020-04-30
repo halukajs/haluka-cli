1a4826944ef45ae4a2e1822e5643de81dc014d4d
Robin Panta <hacktivistic@gmail.com>
2020-04-30 23:47:10 +0530

    feat(run): Add RC File support for cli apps

--- commands/run.js
+++ commands/run.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-dynamic-require */
 
 const chalk = require('chalk');
 const command = require('../lib/commander');
@@ -19,12 +20,30 @@ function loadPackage() {
   return pkg;
 }
 
+function loadRC() {
+  let rc;
+  try {
+    // eslint-disable-next-line global-require
+    rc = require(util.getCurrentPath('.halukacli'));
+  } catch (error) {
+    if (error.code === 'MODULE_NOT_FOUND') {
+      console.error(chalk.redBright('Error: ".halukacli.js" file not found in the project.'));
+    } else {
+      console.error(chalk.redBright('You have error in your .halukacli.js file.'));
+      console.error(chalk.yellowBright(error));
+    }
+    process.exit(1);
+  }
+  return rc;
+}
+
+
 /**
  * Copyright (c) Robin Panta
  * Inspire Command
  */
 module.exports = command({
-  command: 'run <command>',
+  command: 'run [commands...]',
   description: 'Runs commands from active Haluka application.',
 }, (yargv) => {
   const pkg = loadPackage();
@@ -42,7 +61,27 @@ module.exports = command({
 
   // In the application
   console.log(chalk.greenBright(`CLI running inside '${pkg.name}'`));
-  const { command: appCommand } = yargv;
-  console.log(`Attempting to run ${chalk.yellowBright(appCommand)} from '${pkg.name}'.`);
-  // TODO: application cli config and task runner
+  let commands = yargv.commands || ['default'];
+  commands = commands.length === 0 ? ['default'] : commands;
+
+  console.log(`Attempting to run ${chalk.yellowBright(commands.join(', '))} from '${pkg.name}'.`);
+
+  const startTime = Date.now();
+
+  const rc = loadRC();
+  const notFound = commands.filter((x) => !(x in rc));
+
+  if (notFound.length > 0) {
+    console.log(chalk.redBright(`No command registered for '${notFound.join(', ')}'. Check your '.halukacli.js' file and try again.`));
+    process.exit(0);
+  }
+
+  const executables = (commands.map((x) => rc[x])).reverse();
+
+  console.log();
+  // task();
+  console.log();
+
+  const endTime = Date.now();
+  console.log(chalk.greenBright(`Command executed in ${(endTime - startTime) / 1000}s.`));
 });
