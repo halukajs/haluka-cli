86af9764be69ad00879cdeafed184a2d7be64576
Robin Panta <hacktivistic@gmail.com>
2021-11-07 17:49:32 +0530

    feat(new): add create new project from seed

--- commands/new.js
+++ commands/new.js
@@ -1,7 +1,13 @@
 const chalk = require('chalk');
 const fs = require('fs');
-const command = require('../lib/commander');
 const util = require('../lib/util');
+const command = require('../lib/commander');
+
+const prettyPrintError = (error) => {
+  console.error(`\n  ${this.chalk.bgRed.white(' Error ')} ${this.chalk.red(error.message)}\n`);
+};
+
+const SEED_PATH = 'https://github.com/halukadev/seed.git';
 
 /**
  * Copyright (c) Robin Panta
@@ -14,6 +20,13 @@ module.exports = command({
   const projectName = yargv.name;
   if (fs.existsSync(util.getCurrentPath(projectName))) {
     console.error(chalk.red(`Error: Cannot create project. \nA file or folder with name '${projectName}' already exists in the active directory.`));
+    process.exit(1);
+  }
+
+  try {
+    util.createApp(SEED_PATH, projectName);
+  } catch (error) {
+    prettyPrintError(error);
     process.exit(1);
   }
   // safe to create project. Pull project seed from git.
